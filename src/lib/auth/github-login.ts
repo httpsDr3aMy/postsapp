b6f45426b173f562a90cac88715f8189ee9a5add
Kamil <125757644+httpsDr3aMy@users.noreply.github.com>
2024-06-27 17:37:30 +0200

    fix

--- src/lib/auth/github-login.ts
+++ src/lib/auth/github-login.ts
@@ -5,7 +5,7 @@ export async function signInWithGithub() {
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'github',
         options: {
-            redirectTo: `https://postsapp-du5gq7wmo-kamils-projects-85981210.vercel.app/auth/callback`,
+            redirectTo: `https://postsapp-weld.vercel.app/auth/callback`,
         },
     })
 }
